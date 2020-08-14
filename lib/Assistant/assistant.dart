d83df78da3f3417bfe8d9ebe9c2bf9d089c7cf1f
abhinavxx <abhinavsrivastavaazm@gmail.com>
2020-08-14 23:05:00 +0530

    Screen update

--- lib/Assistant/assistant.dart
+++ lib/Assistant/assistant.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:projectBiyt/HubPage/BuyingPage/items_page.dart';
 
 class Assistant extends StatefulWidget {
   @override
